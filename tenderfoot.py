25777b24b49eb196dbca3d71aea286d05f660288
Pradeep Yarlagadda <sreeramsaipradeep@gmail.com>
2018-02-21 17:31:06 +0530

    Add Alpha Beta Proning with Temporary Heuristic

--- tenderfoot.py
+++ tenderfoot.py
@@ -4,7 +4,7 @@ class Tenderfoot():
 
 	def __init__(self):
 		self.INF 	= 1000000
-		self.depth 	= 4
+		self.depth 	= 3
 		self.ply 	= {}
 
 	def move(self, board, old_move, flag):
@@ -16,53 +16,57 @@ class Tenderfoot():
 			self.ply["max"] = "o"
 			self.ply["min"] = "x"
 
-		score = self.minimax(board, self.depth, -self.INF, self.INF, old_move, True)
+		score,move = self.minimax(board, self.depth, -self.INF, self.INF, old_move, True)
 
-		return score[1]
+		return move
 
 
 	def minimax(self, board, depth, alpha, beta, old_move, is_max_ply):
 
-		if depth == 0 or board.find_terminal_state()[0] != "CONTINUE":
+		if depth == 0 or board.find_terminal_state() != ('CONTINUE', '-'):
 			return self.heuristic(board)
 
 		moves = board.find_valid_move_cells(old_move)
 
 		if is_max_ply:
 			max_score = -self.INF
-			max_moves = []
+			max_move  = (-1,-1)
 			for mv in moves:
 				board.update(old_move, mv, self.ply["max"])
 				
-				score = self.minimax(board,depth-1,alpha,beta,mv,False)[0]
-				if(max_score < score):
-					max_score = score
-					max_moves = [mv]
-				elif max_score == score:
-					max_moves.append(mv)
-				
+				score = self.minimax(board, depth-1, alpha, beta, mv, False)[0]
+
 				board.block_status[mv[0]/4][mv[1]/4]= '-'
 				board.board_status[mv[0]][mv[1]] 	= '-'
+
+				alpha = max(alpha,score)
+				if(max_score < score):
+					max_score = score
+					max_move = mv
+				if(alpha >= beta):
+					break
 				
-			return (max_score, random.choice(max_moves))
+			return (max_score, max_move)
 
 		else:
 			min_score = self.INF
-			min_moves = []
+			min_move = (-1,-1)
 			for mv in moves:
 				board.update(old_move, mv, self.ply["min"])
 				
 				score = self.minimax(board,depth-1,alpha,beta,mv,True)[0]
-				if(min_score > score):
-					min_score = score
-					min_moves = [mv]
-				elif min_score == score:
-					min_moves.append(mv)
-				
+
 				board.block_status[mv[0]/4][mv[1]/4]= '-'
 				board.board_status[mv[0]][mv[1]] 	= '-'
+
+				beta = min(beta,score)
+				if(min_score > score):
+					min_score = score
+					min_move  = mv
+				if(alpha >= beta):
+					break
 				
-			return (min_score, random.choice(min_moves))
+			return (min_score, min_move)
 
 	def heuristic(self, board):
 		return (1,(-1,-1))
