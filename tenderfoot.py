abf85d565e966683dfe398cc758f62f71e46eb3f
Subramanyam <subramanyam.kmk03@gmail.com>
2018-02-27 01:51:31 +0530

    new bot added

--- tenderfoot.py
+++ tenderfoot.py
@@ -7,8 +7,6 @@ class Tenderfoot():
 		self.depth 	= 3
 		self.ply 	= {}
 		self.block_weights = [[6, 4, 4, 6], [4, 3, 3, 4], [4, 3, 3, 4], [6, 4, 4, 6]]
-		self.pc = 0
-		self.oc = 0
 		self.winning_comb = []
 		for i in range(4):
 			comb = []
@@ -32,7 +30,10 @@ class Tenderfoot():
 		self.winning_comb.append(comb)
 
 	def move(self, board, old_move, flag):
-	
+
+		# if old_move == (-1, -1):
+		# 	return (4, 4)
+
 		if flag == "x":			
 			self.ply["max"]	= "x"
 			self.ply["min"] = "o"
@@ -40,12 +41,10 @@ class Tenderfoot():
 			self.ply["max"] = "o"
 			self.ply["min"] = "x"
 
-		for row in board.block_status:
-			self.pc += row.count(self.ply["max"])
-			self.oc += row.count(self.ply["min"])
+		self.cntp = sum(blocks.count(self.ply["max"]) for blocks in board.block_status)
+		self.cnto = sum(blocks.count(self.ply["min"]) for blocks in board.block_status)
 
 		score,move = self.minimax(board, self.depth, -self.INF, self.INF, old_move, True)
-
 		return move
 
 
@@ -53,36 +52,7 @@ class Tenderfoot():
 
 		terminal_status = board.find_terminal_state()
 		if depth == 0 or terminal_status != ('CONTINUE', '-'):
-			if terminal_status[1] == 'WON':
-				if terminal_status[0] == self.ply["max"]:
-					return (10, old_move)
-				else:
-					return (-10, old_move)
-			boardmax = self.heuristic(board, self.ply["max"])
-			boardmin = self.heuristic(board, self.ply["min"])
-			for i in range(4):
-				for j in range(4):
-					if boardmax[i][j] == 1:
-						boardmin[i][j] = 0
-					if boardmin[i][j] == 1:
-						boardmax[i][j] = 0
-
-			cnt1 = 0
-			cnt2 = 0
-			for row in board.block_status:
-				cnt1 += row.count(self.ply["max"])
-				cnt2 += row.count(self.ply["min"])
-			gain = self.find_prob_block(boardmax) - self.find_prob_block(boardmin)
-
-			if self.pc < cnt1 and self.oc == cnt2:
-				gain += 0.05
-			elif self.pc < cnt1 and cnt1 - self.pc < cnt2 - self.oc:
-				gain -= 0.02
-			elif cnt1 < self.pc and cnt2 > self.oc:
-				gain -= 0.05
-
-			return (gain, old_move)
-
+			return (self.heuristic(board, self.ply["max"]), old_move)
 
 		moves = board.find_valid_move_cells(old_move)
 
@@ -90,6 +60,7 @@ class Tenderfoot():
 			max_score = -self.INF
 			max_move  = (-1,-1)
 			for mv in moves:
+
 				board.update(old_move, mv, self.ply["max"])
 				
 				score = self.minimax(board, depth-1, alpha, beta, mv, False)[0]
@@ -101,7 +72,7 @@ class Tenderfoot():
 				if(max_score < score):
 					max_score = score
 					max_move = mv
-				if(alpha >= beta):
+				if(alpha > beta):
 					break
 				
 			return (max_score, max_move)
@@ -110,6 +81,7 @@ class Tenderfoot():
 			min_score = self.INF
 			min_move = (-1,-1)
 			for mv in moves:
+
 				board.update(old_move, mv, self.ply["min"])
 				
 				score = self.minimax(board,depth-1,alpha,beta,mv,True)[0]
@@ -121,49 +93,104 @@ class Tenderfoot():
 				if(min_score > score):
 					min_score = score
 					min_move  = mv
-				if(alpha >= beta):
+				if(alpha > beta):
 					break
 				
 			return (min_score, min_move)
 
 	def heuristic(self, board, flag):
-		# return (1,(-1,-1))
+
 		board_prob = [[0 for i in range(4)] for j in range(4)]
 		for i in range(4):
 			for j in range(4):
-				if board.block_status[i][j] == '-': 
-					board_prob[i][j] = self.find_prob_cells(board.board_status, 4*i, 4*j, flag)
-				elif board.block_status[i][j] == flag:
-					board_prob[i][j] = 1
+				board_prob[i][j] = self.find_prob_cells(board.board_status, 4*i, 4*j)
+		prob = self.find_prob_block(board.block_status, board_prob)
 
-		# return self.find_prob_block(board_prob)
-		return board_prob
+		cnt1 = sum(blocks.count(self.ply["max"]) for blocks in board.block_status)
+		cnt2 = sum(blocks.count(self.ply["min"]) for blocks in board.block_status)
+		if self.cntp < cnt1 and cnt2 == self.cnto:
+			prob += 50
+		elif cnt1 > self.cntp and (cnt1 - self.cntp) < (cnt2 - self.cnto):
+			prob -= 20
+		elif cnt1 < self.cntp and cnt2 > self.cnto:
+			prob -= 50
 
-	def find_prob_cells(self, board_status, topx, topy, flag):
+		return prob
 
-		total_prob = 0
+	def find_prob_cells(self, block_status, topx, topy):
+
+		prob = 0
 		for comb in self.winning_comb:
-			prob = 1
-			for (x, y) in comb:
-				if board_status[topx + x][topy + y] == '-':
-					prob *= 0.5
-				elif board_status[topx + x][topy + y] != flag:
-					prob *= 0
-			if prob == 1:
-				return 1
-			total_prob += prob
+			pCount = 0
+			oCount = 0
+			comb_status = [block_status[topx + x][topy + y] for (x, y) in comb]
 
-		return total_prob / 12
+			pCount = comb_status.count(self.ply["max"])
+			oCount = comb_status.count(self.ply["min"])
 
-	def find_prob_block(self, board_prob):
+			if (pCount == 0 and oCount > 0):
+				prob -= pow(10,oCount-1)
+			elif (pCount == 1 and oCount == 3):
+				prob += pow(10,2)
+			elif (oCount == 0 and pCount):
+				prob += pow(10,pCount-1)
+		return prob/1000.0
 
-		total_prob = 0
+	def find_prob_block(self, board_status, board_prob):
+
+		prob = 0
 		for comb in self.winning_comb:
-			prob = 1
-			for (x, y) in comb:
-				prob *= board_prob[x][y]
-			if prob == 1:
-				return 1
-			total_prob += prob
+			pCount = 0
+			oCount = 0
+			combProb = 0
+			comb_status = [board_status[x][y] for (x, y) in comb]
+			pCount = comb_status.count(self.ply["max"])
+			oCount = comb_status.count(self.ply["min"])
+			combProb = sum([board_prob[x][y] for (x, y) in comb])
+			prob = self.normalize(combProb, prob)
+			if (pCount == 0 and oCount > 0):
+				prob -= pow(10,oCount)
+			elif (pCount == 1 and oCount == 3):
+				prob += pow(10,3)
+			elif (oCount == 0 and pCount):
+				prob += pow(10,pCount)
 
-		return total_prob / 12
+		return prob
+
+	def normalize(self, p_gain, gain):
+
+		if p_gain < 1 and p_gain >= -1:
+			gain += p_gain
+		if p_gain >= 3 and p_gain < 4:
+			val = 100
+			val += (p_gain - 3) * 900
+			gain += val
+		if p_gain >= -2 and p_gain < -1:
+			val = -1
+			val -= (abs(p_gain) - 1) * 9
+			gain += val
+		if p_gain < -3 and p_gain >= -4:
+			val = -100
+			val -= (abs(p_gain) - 3) * 900
+			gain += val
+		if p_gain >= 1 and p_gain < 2:
+			val = 1
+			val += (p_gain - 1) * 9
+			gain += val
+		if p_gain >= 4:
+			val = 1000
+			val += (p_gain - 4) * 9000
+			gain += val
+		if p_gain >= 2 and p_gain < 3:
+			val = 10
+			val += (p_gain - 2) * 90
+			gain += val
+		if p_gain < -4:
+			val = -1000
+			val -= (abs(p_gain) - 4) * 9000
+			gain += val
+		if p_gain >= -3 and p_gain < -2:
+			val = -10
+			val -= (abs(p_gain) - 2) * 90
+			gain += val
+		return gain
